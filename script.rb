0c31f3d232f38a95b06a59b39418a88f40f100c0
Noah Gordon <tortuga.ng@gmail.com>
2014-07-28 11:28:57 -0400

    Initial commit.

---
+++ script.rb
@@ -0,0 +1,11 @@
+require 'addressable/uri'
+require 'rest-client'
+require 'json'
+require 'nokogiri'
+
+# google project id: tenacious-coder-655
+
+puts "What is your address?"
+address = gets.chomp
+
+
